f39bfa4bff97133863b27cc9e91aa5394e20e42c
RomainTalbot <romain.talbot.33@gmail.com>
2021-12-23 10:56:04 +0100

    style on Footer component

--- front/spinning-squid/src/components/Footer.vue
+++ front/spinning-squid/src/components/Footer.vue
@@ -1,35 +1,71 @@
 <template>
   <header>
-    <nav class="navFooter">
-      <router-link
-        :to="{
-          name: 'home',
-        }"
-      >
-        <li className="navFooter-link">Nous contacter</li>
-      </router-link>
+    <nav class="nav-footer">
+       <router-link
+          :to="{
+            name: 'home',
+          }"
+          class="nav-footer-link"
+        >
+          Accueil
+        </router-link>
 
-      <router-link
-        :to="{
-          name: 'home',
-        }"
-      >
-        <li class="navFooter-link">Newsletter</li>
-      </router-link>
+        <router-link
+          :to="{
+            name: 'home',
+          }"
+          class="nav-footer-link"
+        >
+          Trouve ton Skatepark
+        </router-link>
 
-      <router-link
-        :to="{
-          name: 'home',
-        }"
-      >
-        <li class="navFooter-link">Spinning Squad</li>
-      </router-link>
+        <router-link
+          :to="{
+            name: 'home',
+          }"
+          class="nav-footer-link"
+        >
+          Trouve ton Matos
+        </router-link>
     </nav>
   </header>
 </template>
 
 <script>
+export default {
+  name: "Footer",
+}
 </script>
 
-<style>
+
+<style lang="scss">
+@import "../assets/style/main.scss";
+
+.nav-footer {
+  // position: fixed;
+  // float: bottom !important;
+  // bottom: 0;
+  background-color: $black;
+  display: flex;
+  justify-content: space-around;
+  height: 70px;
+  padding: 30px;
+  width: 100%;
+  margin-top: 0;
+
+  .nav-footer-link {
+    color: $grey;
+    font-family: 'Righteous';
+  }
+
+  .nav-footer-link--active {
+    color: $white;
+  }
+}
+
+@media (max-width: $smallScreen) {
+  .nav-footer .nav-footer-link {
+    font-size: 0.75em;
+  }
+}
 </style>
